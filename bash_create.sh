048192dac43aff60ba8ecebafc248277c9ea0637
jec261 <jec261@my.utsa.edu>
2021-04-06 20:49:10 -0700

    Changes to be committed: 	modified:   bash_create.sh

--- bash_create.sh
+++ bash_create.sh
@@ -1,7 +1,17 @@
-echo "Please name your file:"
-read file_name
-file=$file_name
+#!/bin/bash
+
+echo "Enter a file name"
+read file
+locate $file | grep -q "."
+
+while [ $? == 0 ]; do
+        echo "File name taken, enter another file name"
+        read file
+        locate $file | grep -q "."
+
+done
 echo "#!/bin/bash" >> $file
 read -p "bash file $file created, Press enter to continue"
 eval chmod "+x" $file
 eval nano $file
+
